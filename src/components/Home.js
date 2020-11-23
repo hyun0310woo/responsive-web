2c332eeee77f7cb14d6edc77c3daa6a7f4d66c2d
김현우 <hyun0310woo@gmail.com>
2020-11-24 02:18:02 +0900

    Add Home.js down-arrow && css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,27 @@
 import React from 'react'
+import { FiChevronsDown } from "react-icons/fi";
 import "../App.css"
 import "./Home.css"
 
+
 function Home() {
     return (
-        <section className="home-container">
-            <img className="backgrund-img" src="images/background-img.jpg" />
+          <section className="home-container">
+            <img className="backgrund-img" src="images/background-img.jpg" alt="backgrund"/>
             <h1>FRONT-END DEVELOPER</h1>
             <hr />
             <p>
-                프론트엔드 개발자의 포트폴리오 사이트
+                프론트엔드 개발자의 포트폴리오 사이트입니다.
             </p>
-        </section>
+            <FiChevronsDown className="down-arrow" size="40" color="#fff"/>
+          </section>
+        
     )
 }
 
 export default Home
+
+
+
+
+
