ce8a42f3beb98b38ef17a89fb8e007145461b39e
김현우 <hyun0310woo@gmail.com>
2020-12-01 04:55:21 +0900

    format

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import  {BrowserRouter as Router, Switch, Route} from "react-router-dom"
 import './App.css';
-import Home from './components/Home'
-import Navbar from './components/Navbar';
-import About from "./components/About"
-import Skills from "./components/Skills"
-import Contact from "./components/Contact"
-import Footer from "./components/Footer"
+
+import Navbar from "./components/Navbar"
+
 
 
 const App = () => {
   return (
-    
-    <div className="App">
-      <Navbar/>
-      <Home />
-      <About />
-      <Skills />
-      <Contact />
-      <Footer />
-    </div>
+    <Navbar />
   )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,66 +1,46 @@
-import React, {useState, useEffect} from 'react'
-import {animateScroll as scroll, Link} from "react-scroll";
+import React, {useState} from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useParams
+  } from "react-router-dom";
 import { FaBars } from "react-icons/fa";
 import { FaTimes } from "react-icons/fa";
-import "./Navbar.css"
+
 
 const Navbar = () => {
     const [click, setClick] = useState(false);
 
     const handleClick = () => {
-        setClick(!click);
+        setClick(!click)
     }
 
-    const closeMobileMenu = () => {
-        setClick(false);
-    }
-
-    const upScroll = () => {
-        scroll.scrollToTop()
-    }
-
-    const [scrollPosition, setSrollPosition] = useState(0);
-
-    const handleScroll = () => {
-      const position = window.pageYOffset;
-      setSrollPosition(position);
-    };
-  
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll, { passive: true });
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
-
-    return (
-            <nav className="navbar">
-                <div className="navbar-container">
-                    <Link to="/" className={scrollPosition <= 700 ? "navbar-logo" : "navbar-logo down"} onClick={upScroll}>
-                        HW
-                    </Link>
-                    <div className={scrollPosition <= 700 ? "menu-icon" : "menu-icon down"} onClick={handleClick}>
-                        {click ? <FaTimes /> : <FaBars />}
-                    </div>
-                    <ul className={click ? "nav-menu active" : "nav-menu"}>
-                        <li className={scrollPosition <= 700 ? "nav-item" : "nav-item down"}>
-                            <Link to="About" className="nav-links" smooth={true} duration={500} onClick={closeMobileMenu}>
-                            ABOUT
-                            </Link>
-                        </li>
-                        <li className={scrollPosition <= 700 ? "nav-item" : "nav-item down"}>
-                            <Link to="Skills" className="nav-links" smooth={true} duration={500} onClick={closeMobileMenu}>
-                                SKILLS
-                            </Link>
-                        </li>
-                        <li className={scrollPosition <= 700 ? "nav-item" : "nav-item down"}>
-                            <Link to="Contact" className="nav-links" smooth={true} duration={500} onClick={closeMobileMenu}>
-                            CONTACT
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
+    return(
+    <Router>  
+        <section className="navbar-container">
+            <nav>
+                <h1>portfolio<span>.</span></h1>
+                <ul>
+                <div onClick={handleClick}>{click ?  <FaTimes /> : <FaBars/>}</div>
+                    <li>
+                        <Link to="/Home">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/Skills">Skills</Link>
+                    </li>
+                    <li>
+                        <Link to="/Projects">Projects</Link>
+                    </li>
+                    <li>
+                        <Link to="/Contacts">Contacts</Link>
+                    </li>
+                </ul>
             </nav>
+        </section>
+        
+    </Router>  
     )
 }
 
