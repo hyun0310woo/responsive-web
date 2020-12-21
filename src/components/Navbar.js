0d27f9afb97cd5d4625440d726f48298e0c2c562
김현우 <hyun0310woo@gmail.com>
2020-12-21 23:43:25 +0900

    Fix Navbar Error

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,22 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import "./Navbar.css"
 
 const Navbar = () => {
-    let prevScrollpos = window.pageYOffset;
-    
-    window.onscroll = function() {
-    let currentScrollPos = window.pageYOffset;
-        if (prevScrollpos > currentScrollPos) {
-            document.querySelector(".nav-top-logo").style.top = "0";
-          } else {
-            document.querySelector(".nav-top-logo").style.top = "-30px";
-         }
-        prevScrollpos = currentScrollPos;
-    }
+      onscroll = () => {
+        if(window.pageYOffset === 0) {
+            document.querySelector(".navbar-container").style.top = "0px";
+        } else {
+            document.querySelector(".navbar-container").style.top = "-70px";
+        }
+      }
     return(
         <nav className="navbar-container">
             <div className="nav-top-logo">WEB DEVELOPER</div>
-            {/* <ul className="navbar-items"> */}
-                {/* <li className="item logo">HW</li> */}
-                {/* <li className="item">Contact</li> */}
-            {/* </ul> */}
+            <ul className="navbar-items">
+                <li className="item">ABOUT</li>
+                <li className="item logo">HW</li>
+                <li className="item">SKILLS</li>
+            </ul>
         </nav>
         )
 }
