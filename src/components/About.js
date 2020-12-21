73b1b0f1e81c629d5f08139cd77e2cfb406724c7
김현우 <hyun0310woo@gmail.com>
2020-12-22 04:09:02 +0900

    Add a tag url

--- src/components/About.js
+++ src/components/About.js
@@ -12,13 +12,13 @@ const About = () => {
                 <img src="images/ImgTest.jpg" width="200px" height="200px" />
                 <div className="user-info">
                     <div>
-                        <div className="user-name">Kim Hyun Woo</div>
-                        <div><RiSmartphoneLine className="icon"/> 010 1234 1234</div>
-                        <div><AiOutlineMail className="icon" /> hyun0301woo212@gmail.com</div>
+                        <div className="user-name">김 현우 - Kim Hyun Woo</div>
+                        <div><RiSmartphoneLine className="icon"/> 010-3877-5174</div>
+                        <div><AiOutlineMail className="icon" /> hyun0310woo@gmail.com</div>
                     </div>
                     <div className="icons">
-                        <div><GoMarkGithub className="icon2"/></div>
-                        <div><FaBloggerB className="icon2"/></div>
+                        <a href="https://github.com/hyun0310woo" target="blank"><GoMarkGithub className="icon2"/></a>
+                        <a href="https://hyun0310woo.tistory.com/" target="blank"><FaBloggerB className="icon2"/></a>
                     </div>
                 </div>
             </div>
