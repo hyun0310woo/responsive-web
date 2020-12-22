ef2a71a4aa3f40e7f18cb78299bb8cc52d17e085
김현우 <hyun0310woo@gmail.com>
2020-12-23 06:11:01 +0900

    Add Project3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Project1 from "./components/Animation-transition1/Project1"
 import Project2 from "./components/Animation-transition2/Project2"
+import Project3 from "./components/Animation-transition3/Project3"
 import "./App.css"
 
 const App = () => {
   return (
     <div className="app">
-      <Project2/>    
+      <Project3 />    
     </div>
   )
 }
---
+++ src/components/Animation-transition3/Project3.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Project3.css"
+
+const Project3 = () => {
+    return (
+        <div className="container">
+            <input type="checkbox" id="menuicon"></input>
+            <label for="menuicon">
+                <span></span>
+                <span></span>
+                <span></span>
+            </label>
+        </div>
+    )
+}
+
+export default Project3
