8071553b248a87f680c783a501dbc9ba2efe4c89
김현우 <hyun0310woo@gmail.com>
2020-12-22 23:36:10 +0900

    Format

--- src/components/About.js
+++ src/components/About.js
@@ -1,27 +1,25 @@
 import React from 'react'
-import { GoMarkGithub } from "react-icons/go";
-import { RiSmartphoneLine } from "react-icons/ri";
-import { FaBloggerB } from "react-icons/fa";
-import { AiOutlineMail } from "react-icons/ai";
 import "./About.css"
 
 const About = () => {
     return(
         <section className="about-container" id="about">
-            <div className="about-info">
+            {/* <div className="about-info">
                 <img src="images/ImgTest.jpg" width="200px" height="200px" />
                 <div className="user-info">
                     <div>
-                        <div className="user-name">김 현우 - Kim Hyun Woo</div>
-                        <div><RiSmartphoneLine className="icon"/> 010-3877-5174</div>
-                        <div><AiOutlineMail className="icon" /> hyun0310woo@gmail.com</div>
+                        <div className="user-name">홍길동 • 123456</div>
+                        <div><i class="fas fa-mobile-alt"></i>010 1234 1234<button>복사</button></div>
+                        <div><i class="far fa-envelope"></i>gmail1234@gmail.com<button>복사</button></div>
                     </div>
                     <div className="icons">
-                        <a href="https://github.com/hyun0310woo" target="blank"><GoMarkGithub className="icon2"/></a>
-                        <a href="https://hyun0310woo.tistory.com/" target="blank"><FaBloggerB className="icon2"/></a>
+                        <a href="https://github.com/hyun0310woo" target="blank"><i class="fab fa-github-square"></i></a>
+                        <a href="https://hyun0310woo.tistory.com/" target="blank"><i class="fab fa-blogger"></i></a>
+                        <a><i class="fab fa-facebook-square"></i></a>
+                        <a><i class="fab fa-instagram-square"></i></a>
                     </div>
                 </div>
-            </div>
+            </div> */}
         </section>
         )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link, animateScroll as scroll } from 'react-scroll'
 import "./Navbar.css"
 
 const Navbar = () => {
+    const [test, setTest] = useState(true);
     const upScroll = () => {
         scroll.scrollToTop()
     }
     
-      onscroll = () => {
-        if(window.pageYOffset === 0) {
-            document.querySelector(".navbar-container").style.top = "0px"
-            document.querySelector(".navbar-container").style.borderBottom = "thin solid white"
+       onscroll = () => {
+        if(!window.pageYOffset) {
+            document.querySelector(".navbar-container").style.top = "0"
+            setTest(true);
+            // document.querySelector(".navbar-container").style.borderBottom = "thin solid #111111"
         } else {
             document.querySelector(".navbar-container").style.top = "-70px"
-            document.querySelector(".navbar-container").style.borderBottom = "thin solid lightgray"
+            setTest(false);
+            // document.querySelector(".navbar-container").style.borderBottom = "thin solid gray"
         }
       }
+      
+      console.log("???: ",test)
     return(
         <nav className="navbar-container">
             <h1 className="nav-top-logo">Hello I'm WEB DEVELOPER.</h1>
             <ul className="nav-menu">
                     <li>
-                        <Link className="nav-item logo" onClick={upScroll} smooth={true}>HW</Link>
-                    </li>
-                    <li>
-                        <Link className="nav-item" to="about" smooth={true}>ABOUT</Link>
+                        <Link className="nav-item" to="about"  onClick={upScroll}  smooth={true}>ABOUT</Link>
                     </li>
                     <li>
                         <Link className="nav-item" to="skills" smooth={true}>SKILLS</Link>
