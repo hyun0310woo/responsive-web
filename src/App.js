dadc55317b6ec0c41076916bbff8e33381337929
김현우 <hyun0310woo@gmail.com>
2020-12-23 05:25:54 +0900

    Add Project2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Project1 from "./components/Animation-transition/Project1"
+import Project1 from "./components/Animation-transition1/Project1"
+import Project2 from "./components/Animation-transition2/Project2"
 import "./App.css"
 
 const App = () => {
   return (
     <div className="app">
-      <Project1/>    
+      <Project2/>    
     </div>
   )
 }
---
+++ src/components/Animation-transition2/Project2.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Project2.css"
+
+const Project2 = () => {
+    return (
+        <div>Project2</div>
+    )
+}
+
+export default Project2
