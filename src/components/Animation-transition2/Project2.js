288b028c3124886cdd55ba2c35ffde5d6bda6390
김현우 <hyun0310woo@gmail.com>
2020-12-23 05:49:48 +0900

    Fix Project2

--- src/components/Animation-transition2/Project2.js
+++ src/components/Animation-transition2/Project2.js
@@ -3,7 +3,68 @@ import "./Project2.css"
 
 const Project2 = () => {
     return (
-        <div>Project2</div>
+            <ul className="imglist_ul">
+                <li>
+                    <a href="#">  
+                        <div className="screen">
+                            <span></span><span></span><span></span><span></span>
+                            <img src="http://placehold.it/150x90" />
+                        </div>
+                        <h3>Project2</h3>
+                        <p>Project2 test code</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">  
+                        <div className="screen">
+                            <span></span><span></span><span></span><span></span>
+                            <img src="http://placehold.it/150x90" />
+                        </div>
+                        <h3>Project2</h3>
+                        <p>Project2 test code</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">  
+                        <div className="screen">
+                            <span></span><span></span><span></span><span></span>
+                            <img src="http://placehold.it/150x90" />
+                        </div>
+                        <h3>Project2</h3>
+                        <p>Project2 test code</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">  
+                        <div className="screen">
+                            <span></span><span></span><span></span><span></span>
+                            <img src="http://placehold.it/150x90" />
+                        </div>
+                        <h3>Project2</h3>
+                        <p>Project2 test code</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">  
+                        <div className="screen">
+                            <span></span><span></span><span></span><span></span>
+                            <img src="http://placehold.it/150x90" />
+                        </div>
+                        <h3>Project2</h3>
+                        <p>Project2 test code</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">  
+                        <div className="screen">
+                            <span></span><span></span><span></span><span></span>
+                            <img src="http://placehold.it/150x90" />
+                        </div>
+                        <h3>Project2</h3>
+                        <p>Project2 test code</p>
+                    </a>
+                </li>
+            </ul>
     )
 }
 
