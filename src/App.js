c2f80229ce86d1f2d44120f8790fe136a9777e50
김현우 <hyun0310woo@gmail.com>
2020-12-23 06:24:10 +0900

    Add Project4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import Project1 from "./components/Animation-transition1/Project1"
-import Project2 from "./components/Animation-transition2/Project2"
-import Project3 from "./components/Animation-transition3/Project3"
+// import Project1 from "./components/transition1/Project1"
+// import Project2 from "./components/transition2/Project2"
+// import Project3 from "./components/transition3/Project3"
+import Project4 from "./components/transition4/Project4"
+
 import "./App.css"
 
 const App = () => {
   return (
     <div className="app">
-      <Project3 />    
+      <Project4 />    
     </div>
   )
 }
---
+++ src/components/transition4/Project4.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Project4.css"
+
+const Project4 = () => {
+    return (
+        <div className="container">
+            <input type="checkbox" id="menuicon" />
+            <label for="menuicon">
+                <span></span>
+                <span></span>
+                <span></span>
+            </label>
+            <div className="sidebar">
+
+            </div>
+        </div>
+    )
+}
+
+export default Project4
