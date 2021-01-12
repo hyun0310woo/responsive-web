9d9ee9664f887daaa3b4283de7208aaf26fe8c3f
김현우 <hyun0310woo@gmail.com>
2021-01-12 18:37:57 +0900

    Fix toy05

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import React from 'react';
 // import Project2 from "./components/transition2/Project2"
 // import Project3 from "./components/transition3/Project3"
 // import Project4 from "./components/transition4/Project4"
-// import Project5 from "./components/transition5/Project5"
-import Project6 from "./components/transition6/Project6"
+import Project5 from "./components/transition5/Project5"
+// import Project6 from "./components/transition6/Project6"
 import "./App.css"
 
 const App = () => {
   return (
     <div className="app">
-      <Project6 />    
+      <Project5 />    
     </div>
   )
 }
