aedf47d56791eac8d193b09194f82fea13e60d74
김현우 <hyun0310woo@gmail.com>
2021-01-21 23:41:58 +0900

    Add handleClickFn

--- src/components/toy02/Toy02.js
+++ src/components/toy02/Toy02.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./Toy02.css" 
 
 const Toy02 = () => {
+  const [onClick, setOnClick] = useState(false);
+
+  const handleClick = () => {
+    setOnClick(!onClick);
+  }
+
     return (
       <div id="container">
+        {/* <label>
+          <input type="radio" name="btn" ></input>
+        </label> */}
         <label>
-          <input type="radio" name="btn"></input>
-        </label>
-        <label>
-          <input type="radio" name="btn"></input>
+          <input onClick={handleClick} type="radio" name="btn" checked={onClick}></input>
         </label>
 
       </div>
