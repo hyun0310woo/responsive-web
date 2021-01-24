1680ecc19e7a6589fa69151e379adfde2c1936f1
김현우 <hyun0310woo@gmail.com>
2021-01-24 20:41:51 +0900

    Add icons

--- src/components/toy03/Toy03.js
+++ src/components/toy03/Toy03.js
@@ -8,6 +8,23 @@ const Toy03 = () => {
         <div className="color"></div>
         <div className="color"></div>
         <div className="color"></div>
+        <ul>
+          <li>
+            <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+          </li>
+          <li>
+            <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+          </li>
+          <li>
+            <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+          </li>
+          <li>
+            <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+          </li>
+          <li>
+            <a href="#"><i class="fa fa-whatsapp" aria-hidden="true"></i></a>
+          </li>
+        </ul>
       </section>
     </div>
   )
