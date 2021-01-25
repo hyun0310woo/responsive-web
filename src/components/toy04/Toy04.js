c8b9bc1534846a33bfebd94f1788a8e39ce98bc4
김현우 <hyun0310woo@gmail.com>
2021-01-25 23:59:37 +0900

    Add HTML

--- src/components/toy04/Toy04.js
+++ src/components/toy04/Toy04.js
@@ -3,7 +3,25 @@ import "./Toy04.css"
 
 const Toy04 = () => {
   return(
-    <div>toy04</div>
+    <div>
+      <a className="btn">Subscribe Us</a>
+      <div id="popup">
+        <div className="content">
+          <img src=""></img>
+          <h2>Join Our Newsletter</h2>
+          <p>
+            Lorem, ipsum....dolor..sit amet...
+            show me the money
+          </p>
+          <div className="inputBox">
+            <input type="email" placeholder="Enter Email"></input>
+          </div>
+          <div className="inputBox">
+            <input type="submit" value="Sign Up"></input>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
